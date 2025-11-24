2dd7316461a9062e37941a49fe8adcf03cee0cf9
Mohammad Miadh Angkad <MAngkad.BSDSBA2027@aim.edu>
2025-11-25 04:17:49 +0800

    Add env

--- tljh/jupyterhub_config.py
+++ tljh/jupyterhub_config.py
@@ -30,6 +30,11 @@ c.SystemdSpawner.default_shell = "/bin/bash"
 # Drop the '-singleuser' suffix present in the default template
 c.SystemdSpawner.unit_name_template = "jupyter-{USERNAME}"
 
+# Set default environment variables for all users
+c.Spawner.environment = {
+    "CUDA_VISIBLE_DEVICES": "-1",
+}
+
 tljh_config = configurer.load_config()
 configurer.apply_config(tljh_config, c)
 
