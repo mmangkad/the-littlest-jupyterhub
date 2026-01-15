8c7c9ba6a897194e00a6a81d49bd2ba843a4aeae
Mohammad Miadh Angkad <mangkad.bsdsba2027@aim.edu>
2026-01-15 11:41:49 +0800

    merge (#1)
    * Bump actions/checkout from 5 to 6
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 5 to 6.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v5...v6)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-version: '6'
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Enclose ${PATH} in double-quotes
    
    to avoid problems with white space.
    
    Closes https://github.com/jupyterhub/the-littlest-jupyterhub/issues/1054
    
    * Update `traefik` to `3.6.5` (#1058)
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Min RK <benjaminrk@gmail.com>
    Co-authored-by: Raniere Gaia Costa da Silva <Raniere.CostadaSilva@gesis.org>
    Co-authored-by: Raniere Silva <r.gaia.cs@gmail.com>
    Co-authored-by: Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
    Co-authored-by: Simon Li <orpheus+devel@gmail.com>

--- tljh/traefik.py
+++ tljh/traefik.py
@@ -29,13 +29,13 @@ else:
     plat = None
 
 # Traefik releases: https://github.com/traefik/traefik/releases
-traefik_version = "3.6.2"
+traefik_version = "3.6.5"
 
 # record sha256 hashes for supported platforms here
 # checksums are published in the checksums.txt of each release
 checksums = {
-    "linux_amd64": "edc04e863795d741bdba8d70fbc1fd721b16edace829f8f25b0e13a9a34cb134",
-    "linux_arm64": "89e93dca8651163f9d49302e0b2ad7d16a4beb0ed0d04d63443e8ab2c3e77d2f",
+    "linux_amd64": "a9d891026496e0e16cbe4e0599339487411c9876246238c06bbe1a532350a096",
+    "linux_arm64": "801449a26d1389aa60bbb96e79a8ecc8c385b2e76b1dcaf487fd712c427b2831",
 }
 
 _tljh_path = Path(__file__).parent.resolve()
